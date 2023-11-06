bb1289d5468f992abfa9f8320b572729f90fcf0a
Asha <satpathyasharani@gmail.com>
2023-11-06 10:37:33 +0530

    assignmemt 3, 1 question done

--- Assignment3/duplicate.cpp
+++ Assignment3/duplicate.cpp
@@ -1,35 +1,74 @@
-// missing element froim an array with duplicates 
+// // missing element froim an array with duplicates 
 #include <iostream>
 using namespace std;
 
 void findMissing(int *a, int n) {
-    //visited method
-    for(int i = 0; i < n; i++){
-        int index = abs(a[i]);
-        if(a[index - 1] > 0){
-            a[index - 1] *= -1;
+//     //visited method
+//     for(int i = 0; i < n; i++){
+//         int index = abs(a[i]);
+//         if(a[index - 1] > 0){
+//             a[index - 1] *= -1;
+//         }
+//     }
+
+//     // for(int i = 0; i < n; i++){
+//     //     cout << a[i] << " "; 
+//     // } 
+
+//     //all positive indexs are missing
+//     for(int i = 0; i < n; i++){
+//         if(a[i] > 0)
+//             cout << i + 1 << " ";
+//     }
+
+// }
+
+
+// int main()
+// {
+//     int n;
+//     int a[] = {1,3,0,3,3};
+//     n = sizeof(a)/sizeof(int);
+//     findMissing(a,n);
+
+//     return 0;
+// }
+
+//sorting +swapping algo
+
+
+int i = 0;
+while(i<n){
+    int index = a[i] - 1;
+    if(a[i] != a[index]){
+        swap(a[i], a[index]);
+    }
+    else{
+        ++i;
+    }
+    
+}
+// for(int i = 0; i < n; i++){
+//     cout << a[i] << " ";
+// }
+// cout << endl;
+for(int i = 0; i < n; i++){
+    if(a[i] != i+1){
+        cout << i+1 << " ";
         }
     }
-
-    // for(int i = 0; i < n; i++){
-    //     cout << a[i] << " ";
-    // }
-
-    //all positive indexs are missing
-    for(int i = 0; i < n; i++){
-        if(a[i] > 0)
-            cout << i + 1 << " ";
-    }
-
+    cout << endl;
 }
-
 
 int main()
 {
+{
     int n;
-    int a[] = {1,3,0,3,3};
+    int a[] = {1,3,5,3,4};
     n = sizeof(a)/sizeof(int);
     findMissing(a,n);
 
+    return 0;
+}
     return 0;
 }
