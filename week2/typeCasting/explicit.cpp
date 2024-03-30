4598c9242793c6907f1c774de0dc24c32cf99f43
Asha <satpathyasharani@gmail.com>
2024-03-30 21:20:03 +0530

    type casting done

---
+++ week2/typeCasting/explicit.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // "Explic type casting float to integer: 
+    // int a = 10;
+    // float b = 99.8;
+    // float sum = a + (int)b;
+    // cout << sum :  << sum << endl;
+
+    // "Explic type casting flaot to char: 
+    // float c =  65.35;
+    // char ch = (char)c;
+    // cout << ch << endl;
+
+
+    int a = 10;
+    int b = 5;
+    cout << a/b << endl;
+
+    int k = 10;
+    float l = 8.5;
+    cout << k/l << endl;
+
+    float x = 10.23;
+    float y = 3.5;
+    cout << x/y << endl;
+
+
+
+
+
+    return 0;
+}
---
+++ week2/typeCasting/implicit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int num1 = 10;
+    // float num2 = 5.5;
+    // float result1 = num1 + num2;
+    // cout << "int to float " << result1 << endl; 
+
+    // float num3 = 1.0923;
+    // int num4 = 1000;
+    // int result2 = num3+num4;
+    // cout << "float to int " << result2 << endl;
+
+
+    // char ch = 'A';
+    // int a = ch + 1 ; 
+    // cout << "char to int " << a << endl; 
+
+    int a = 97;
+    char ch = a;
+    cout << "int to char " << a << endl;
+
+
+    return 0;
+}
